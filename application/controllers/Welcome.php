4b863e6e4d5296b7903617ac93445cdc393f3326
Hari <Hari@Hari-PC>
2017-08-03 20:15:39 +0530

    1st level code

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+require APPPATH.'/libraries/REST_Controller.php';
+class Welcome extends REST_Controller  {
+	function Welcome() {
+		parent::__construct();
+		$this->load->model('usecase/welcomeusecase','wus',TRUE);
+	}
+	
+	public function index_get()
 	{
 		$this->load->view('welcome_message');
 	}
+	
+	public function welcome_get()
+	{
+		$response = $this->wus->welcome();
+		print_r(json_encode($response));
+	}
+	
 }
+?>
