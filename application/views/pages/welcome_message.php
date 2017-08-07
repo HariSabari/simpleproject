f624e07b8c5282ea77399ae1f0cc44be5d0a3eb4
Hari <Hari@Hari-PC>
2017-08-07 10:34:44 +0530

    Delete

--- application/views/pages/welcome_message.php
+++
@@ -1,9 +0,0 @@
-<div ng-app="login" ng-controller="logincontroller">
-	<h1>Welcome to CodeIgniter!</h1>
-	<button ng-click="logout()">logout</button>
-	<script type="text/javascript">
-		var baseurl = '<?php print base_url();?>';
-	</script>
-	<script type="text/javascript" src="<?php print base_url();?>js/angularjs/angular.min.js"></script>
-	<script type="text/javascript" src="<?php print base_url();?>js/controllerjs/login.js"></script>
-</div>
